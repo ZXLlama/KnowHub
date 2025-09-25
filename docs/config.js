b801e994653ced5c0d970672bdabb3d51308f38d
ZXLlama <ytllama1225@gmail.com>
2025-09-25 23:35:24 +0800

    更新內容

---
+++ docs/config.js
@@ -0,0 +1,26 @@
+const config = {
+  grid: {
+    rows: 1,
+    columns: 1
+  },
+  tiles: [
+    {
+      title: "📚 強制灌入知識",
+      subtitle: "哥們你看過這個後還是不會我就真的救不了你了",
+      image: "./images/DEV.png",
+      link: "./knowledge.html"
+    },
+    {
+      title: "📝 英文單字",
+      subtitle: "這些是作者我不熟的單字們，呵🗿",
+      image: "./images/DEV.png",
+      link: "./vocab.html"
+    },
+    {
+      title: "🧠 各種筆記",
+      subtitle: "整理過的筆記清單，不只有作者我的，還有偷我同學的",
+      image: "./images/DEV.png",
+      link: "./notes.html"
+    }
+  ]
+};
